7de787ed79685f680747d514c00800ade4017cac
Leonardo Rodrigues <leonardojrr1@gmail.com>
2020-02-02 19:04:15 -0400

    organizing the project

---
+++ src/screens/containers/login.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  View,
+  Image,
+  StyleSheet,
+  Text,
+  SafeAreaView
+} from 'react-native';
+
+const Login = () => {
+  return (
+    <View>
+      <SafeAreaView>
+        
+      </SafeAreaView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+
+})
+
+export default Login;
---
+++ src/screens/containers/regist.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {
+  Text
+} from 'react-native';
+
+const Regist = () => {
+  return (
+    <Text>Hola</Text>
+  );
+};
+
+export default Regist;
