00f61d10db0da3295b61a1cd35ce144839db95d9
Leonardo Rodrigues <leonardojrr1@gmail.com>
2020-02-03 02:13:19 -0400

    Adding the fonts and starting with the login screen

--- src/screens/containers/login.js
+++ src/screens/containers/login.js
@@ -9,15 +9,48 @@ import {
 
 const Login = () => {
   return (
-    <View>
-      <SafeAreaView>
-        
-      </SafeAreaView>
+    <View style={styles.body}>
+     <SafeAreaView style={styles.body}>
+        <View style={styles.form_container}>
+          <View style={styles.logo}>
+            <Image source={require("../../../assets/svg/logo.png")}/>
+            <Text style={styles.logo_font}>Crello</Text>
+          </View>
+          
+        </View>
+     </SafeAreaView>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+    body:{
+      width: "100%",
+      height: "100%",
+      backgroundColor:"#e3e3e3",
+      display:"flex",
+      justifyContent:"center",
+      alignItems:"center"
+    },
+    form_container:{
+      width:"65%",
+      height:"75%",
+      backgroundColor:"white",
+      borderRadius: 15,
+      display:"flex",
+      alignItems:"center",
+    },
+    logo:{
+      width: "100%",
+      height:"30%",
+      display:"flex",
+      alignItems:"center"
+    },
+    logo_font:{
+      fontFamily:"Ginchiest",
+      color:"#ff8300",
+      fontSize:30
+    }
 
 })
 
